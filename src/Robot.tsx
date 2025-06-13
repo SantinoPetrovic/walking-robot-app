906de92492df1c16932281ade056afc05fdea76c
santino <santinopetrovic@gmail.com>
2025-06-13 14:13:25 +0200

    Setup Robot with function and types

---
+++ src/Robot.tsx
@@ -0,0 +1,24 @@
+type Direction = 'N' | 'E' | 'S' | 'W';
+
+interface Position {
+  x: number;
+  y: number;
+  direction: Direction;
+}
+
+interface Room {
+  width: number;
+  height: number;
+}
+
+const directions: Direction[] = ['N', 'E', 'S', 'W'];
+
+const Robot = (
+  room: Room,
+  position: Position,
+  commands: string
+) => {
+  return;
+}
+
+export default Robot
