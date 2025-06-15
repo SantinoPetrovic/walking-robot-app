6a94d47755c56e6849b75dbf49858342b764c2c0
santino <santinopetrovic@gmail.com>
2025-06-15 19:44:18 +0200

    Changed some min values for Robot console input fields

--- src/domain/robot/components/RobotConsole.tsx
+++ src/domain/robot/components/RobotConsole.tsx
@@ -68,14 +68,14 @@ const RobotConsole = () => {
         <div className="flex gap-2">
           <input
             type="number"
-            min={1}
+            min={0}
             value={startX}
             onChange={(e) => setStartX(Number(e.target.value))}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
           />
           <input
             type="number"
-            min={1}
+            min={0}
             value={startY}
             onChange={(e) => setStartY(Number(e.target.value))}
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
